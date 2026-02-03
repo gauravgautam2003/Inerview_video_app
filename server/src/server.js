f3329b651f5d33262f9bfa8696719e017d235c16
gauravgautam2003 <gauravgautam9865@gmail.com>
2026-02-03 22:23:47 +0530

    db and tools setup

--- server/src/server.js
+++ server/src/server.js
@@ -1,29 +1,41 @@
 import express from "express";
 import path from "path"
 import { ENV } from "./lib/env.js";
-import { fileURLToPath } from 'url';
+import { connectDB } from "./lib/db.js";
+// import { fileURLToPath } from 'url';
 
 const app = express();
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const __dirname = path.resolve();
+// const __dirname = path.resolve(fileURLToPath(import.meta.url));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/health", (req, res) => {
-    res.status(200).json({message: "server is running!"})
+    res.status(200).json({ message: "server is running!" })
 })
 
 
 // make our app ready for deployment
 
-if(ENV.NODE_ENV === "production"){
+if (ENV.NODE_ENV === "development") {
     app.use(express.static(path.join(__dirname, "../client/dist")));
 
     app.get("/{*any}", (req, res) => {
         res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
     });
 }
-app.listen(ENV.PORT, () => {
-    console.log(`http://localhost:${ENV.PORT}`);
-})
+
+const startServer = async () => {
+    try {
+        await connectDB();
+        app.listen(ENV.PORT, () => {
+            console.log(`http://localhost:${ENV.PORT}`);
+        })
+    } catch (error) {
+        console.log("Error starting the server", error);
+    }
+}
+
+startServer();
